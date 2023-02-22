6dba39b82577a69d4d9f93559922ec079997a4bb
dedysetyo <dedy.setyo@gmail.com>
2023-02-22 22:43:03 +0700

    add subscription

--- src/app.js
+++ src/app.js
@@ -3,14 +3,15 @@ import { Amplify, API, graphqlOperation } from "aws-amplify";
 import awsconfig from "./aws-exports";
 import { createListSettings } from "./graphql/mutations";
 import { listListSettings } from "./graphql/queries";
+import { onCreateListSettings } from "./graphql/subscriptions";
 
 Amplify.configure(awsconfig);
 
 async function createNewTodo() {
   const todo = {
-    list_name: "Your List name",
+    list_name: "Dedy Test List",
     description: `Realtime and Offline (${new Date().toLocaleString()})`,
-    moderator_email: "asdf@jklm.com"
+    moderator_email: "dedy@test.com"
   };
 
   return await API.graphql(graphqlOperation(createListSettings, { input: todo }));
@@ -27,11 +28,19 @@ async function getData() {
 const MutationButton = document.getElementById("MutationEventButton");
 const MutationResult = document.getElementById("MutationResult");
 const QueryResult = document.getElementById("QueryResult");
+const SubscriptionResult = document.getElementById("SubscriptionResult");
 
 MutationButton.addEventListener("click", (evt) => {
   createNewTodo().then((evt) => {
     MutationResult.innerHTML += `<p>${evt.data.createListSettings.name} - ${evt.data.createListSettings.description}</p>`;
   });
+});
+
+API.graphql(graphqlOperation(onCreateListSettings)).subscribe({
+  next: (evt) => {
+    const list = evt.value.data.onCreateListSettings;
+    SubscriptionResult.innerHTML += `<p>${list.name} - ${list.description}</p>`;
+  },
 });
 
 getData();
